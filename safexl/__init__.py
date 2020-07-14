d6148737d91ab74536f152b31f195ccf956b4161
ThePoetCoder <39857885+ThePoetCoder@users.noreply.github.com>
2020-07-14 10:33:45 -0500

    Initial Upload - safexl/__init__.py

---
+++ safexl/__init__.py
@@ -0,0 +1,11 @@
+# Copyright (c) 2020 safexl
+
+from safexl.toolkit import *
+import safexl.xl_constants as xl_constants
+import safexl.colors as colors
+
+
+__author__ = "Eric Smith"
+__email__ = "ThePoetCoder@gmail.com"
+__license__ = "MIT"
+__version__ = "0.1"
